15e7617595cd803c6073f7f0364acadce0438d93
JJ <62784575+jonathanjuarez@users.noreply.github.com>
2020-04-23 21:57:46 +0800

    Add files via upload

---
+++ build/precache-manifest.1271382765ff57402533465f8af42770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8aca0b10329e7e72cee24f5f3ebe9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "c4f132eaa13a8a766674",
+    "url": "/static/css/main.20bc1517.chunk.css"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f132eaa13a8a766674",
+    "url": "/static/js/main.5bfd6f6c.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  }
+]);
